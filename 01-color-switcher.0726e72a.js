f3dd5e60489337e1b65c40fe0816239cb374b6ae
VikaSemenchuk <111804432+VikaSemenchuk@users.noreply.github.com>
2023-02-04 14:44:27 +0000

    Deploying to gh-pages from @ VikaSemenchuk/goit-js-hw-09@8fe012ce511658f6c67b8f1dfe5ccf3d3342b588 🚀

---
+++ 01-color-switcher.0726e72a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0;let n=null;function o(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(){n=setInterval(o,1e3),e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(n),d.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.0726e72a.js.map
