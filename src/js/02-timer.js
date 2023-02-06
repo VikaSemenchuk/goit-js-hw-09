5c81f892bc54a16fcc324b8145d375b130ff0c53
VikaSemenchuk <viktoriia.semenchuk@gmail.com>
2023-02-06 05:12:11 +0100

    task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,11 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const customerInputEl = document.getElementById('datetime-picker');
 
 const startBtnEl = document.querySelector('button[data-start]');
-
-const containerEl = document.querySelector('.timer');
-const fieldsEl = document.querySelectorAll('.field');
-const dateValuesEl = document.querySelectorAll('.value');
-const dateLabelEl = document.querySelectorAll('.label');
 
 const dayEl = document.querySelector('span[data-days]');
 const hourEl = document.querySelector('span[data-hours]');
@@ -17,7 +13,6 @@ const minEl = document.querySelector('span[data-minutes]');
 const secEl = document.querySelector('span[data-seconds]');
 
 startBtnEl.disabled = true;
-// let ms = null;
 let selectedDate = null;
 let intervalId = null;
 const options = {
@@ -27,23 +22,23 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     selectedDate = selectedDates[0];
-    ms = selectedDate < options.defaultDate;
+    ms = selectedDate - options.defaultDate;
 
-    if (ms) {
+    if (ms < 0) {
       Report.warning('Please choose a date in the future');
     } else startBtnEl.disabled = false;
+
     return;
   },
 };
 
 flatpickr(customerInputEl, options);
 
-console.log(selectedDate);
-
 startBtnEl.addEventListener('click', startTimer);
 
 function convertMs(ms) {
-ms = selectedDate - Date.now();
+  ms = selectedDate - Date.now();
+
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
@@ -57,46 +52,57 @@ ms = selectedDate - Date.now();
   // Remaining minutes
   minEl.textContent = addZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  secEl.textContent = addZero(Math.floor((((ms % day) % hour) % minute) / second));
-
+  secEl.textContent = addZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
   // dayEl.textContent = days;
   // hourEl.textContent = hours;
   // minEl.textContent = minutes;
   // secEl.textContent = seconds;
+  if (ms < 1000) {
+    customerInputEl.disabled = false;
+    Notify.success('Time is up!');
+    clearInterval(intervalId);
+  }
 }
 
 function startTimer() {
-  convertMs();
+  // convertMs();
+  customerInputEl.disabled = true;
+
   intervalId = setInterval(convertMs, 1000);
   console.log('work');
   console.log(intervalId);
-startBtnEl.disabled = true;
 
-
+  startBtnEl.disabled = true;
 }
 
-function addZero (number) {
+function addZero(number) {
   return String(number).padStart(2, 0);
 }
 
-containerEl.style.display = 'flex';
-containerEl.style.gap = '14px';
-containerEl.style.marginTop = '14px';
-containerEl.style.heigth = '14px';
+// const containerEl = document.querySelector('.timer');
+// const fieldsEl = document.querySelectorAll('.field');
+// const dateValuesEl = document.querySelectorAll('.value');
+// const dateLabelEl = document.querySelectorAll('.label');
 
-for (const field of fieldsEl) {
-  field.style.display = 'grid';
-  field.style.justifyItems = 'center';
-}
+// containerEl.style.display = 'flex';
+// containerEl.style.gap = '14px';
+// containerEl.style.marginTop = '14px';
+// containerEl.style.heigth = '14px';
 
-for (const value of dateValuesEl) {
-  value.style.fontSize = '28px';
-  value.style.fontFamily = 'sans-serif';
-}
+// for (const field of fieldsEl) {
+//   field.style.display = 'grid';
+//   field.style.justifyItems = 'center';
+// }
 
-for (const label of dateLabelEl) {
-  label.textContent = label.textContent.toUpperCase();
-  label.style.fontSize = '10px';
-  label.style.fontFamily = 'sans-serif';
-}
+// for (const value of dateValuesEl) {
+//   value.style.fontSize = '28px';
+//   value.style.fontFamily = 'sans-serif';
+// }
 
+// for (const label of dateLabelEl) {
+//   label.textContent = label.textContent.toUpperCase();
+//   label.style.fontSize = '10px';
+//   label.style.fontFamily = 'sans-serif';
+// }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+
+  event.preventDefault();
+
+  const firstDelay = Number(document.getElementsByName('delay')[0].value);
+  const stepDelay = Number(document.getElementsByName('step')[0].value);
+  const amountEl = document.getElementsByName('amount')[0].value;
+
+  for (let i = 0; i < amountEl; i += 1) {
+    let position = i + 1;
+    let delay = firstDelay + stepDelay * i;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        // Notiflix.Notify.closeButton = true
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
